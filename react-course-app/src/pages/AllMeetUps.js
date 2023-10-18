823b911fa63099c7d3d4deb7c4cca4a87ac5f6c9
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2023-10-18 18:54:50 +0200

    fetch data and render

--- react-course-app/src/pages/AllMeetUps.js
+++ react-course-app/src/pages/AllMeetUps.js
@@ -1,25 +1,41 @@
 import MeetupList from "../components/meetups/MeetupList"
-const DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description: "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description: "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-]
+import { useState, useEffect } from "react"
+
 export default function AllMeetUpsPage() {
+  const [isloading, setIsLoading] = useState(true)
+  const [allMeetup, setAllMeetups] = useState([])
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetch("https://meetup-c8525-default-rtdb.firebaseio.com/meetups.json")
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        const meetups = []
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          }
+
+          meetups.push(meetup)
+        }
+        setIsLoading(false)
+        setAllMeetups(meetups)
+      })
+  }, [])
+
+  isloading && (
+    <section>
+      <p>Loading...</p>
+    </section>
+  )
+
   return (
     <section>
       <h1>All Meetups</h1>
-      <MeetupList meetups={DATA} />
+      <MeetupList meetups={allMeetup} />
     </section>
   )
 }
--- react-course-app/src/pages/NewMeetUp.js
+++ react-course-app/src/pages/NewMeetUp.js
@@ -1,8 +1,11 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm"
-
+import { useHistory } from "react-router-dom"
 export default function NewMeetUpPage() {
+  const history = useHistory()
   function dataHandler(data) {
-    fetch("https://meetup-c8525-default-rtdb.firebaseio.com/meetups.json", { method: "POST", body: JSON.stringify(data), headers: { "Content-Type": "application/json" } })
+    fetch("https://meetup-c8525-default-rtdb.firebaseio.com/meetups.json", { method: "POST", body: JSON.stringify(data), headers: { "Content-Type": "application/json" } }).then(() => {
+      history.replace("/")
+    })
   }
 
   return (
