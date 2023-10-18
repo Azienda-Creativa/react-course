a1870e301b4af660f90ba2112aac27f68a81044e
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2023-10-18 23:20:19 +0200

    pages, routes and Links

---
+++ nextjs-course-code/pages/portfolio/[projectID].js
@@ -0,0 +1,14 @@
+import React from "react"
+import { useRouter } from "next/router"
+
+export default function PorfolioProjectPage() {
+  const router = useRouter()
+
+  console.log(router.pathname)
+  console.log(router.query)
+  return (
+    <div>
+      <h1>Porfolio Project Page</h1>
+    </div>
+  )
+}
---
+++ nextjs-course-code/pages/portfolio/index.js
@@ -0,0 +1,7 @@
+export default function PortfolioPage() {
+  return (
+    <div>
+      <h1>Porfolio</h1>
+    </div>
+  )
+}
